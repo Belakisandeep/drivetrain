b843fd64c45d8317ada007f344994ad7d8378126
nidhishmehra <nidhish@iqlect.com>
2023-09-04 18:01:59 +0530

    tag added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,7 +13,7 @@ export const metadata = {
 export default function Layout({ children }) {
   return (
     <html lang="en" className="h-full bg-orange-400 text-base antialiased">
-      <Script id="bing" strategy="lazyOnload">
+      <Script id="bing" strategy="afterInteractive">
         {`(function(w,d,t,r,u)
           {
               var f,n,i;
@@ -31,8 +31,20 @@ export default function Layout({ children }) {
           })
           (window,document,"script","//bat.bing.com/bat.js","uetq");`}
       </Script>
+      <Script id="gt" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WS4Q9DGK');`}
+      </Script>
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WS4Q9DGK" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
       </body>
     </html>
   )
