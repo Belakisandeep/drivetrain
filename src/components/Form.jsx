d364894750b83b993d55f2f8f1f2d7a022783afb
sandeep98458 <sandeep@iqlect.com>
2024-12-09 15:16:03 +0530

    scriptchange

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -551,32 +551,32 @@ const FormComponent = ({ part = 'Part' }) => {
         </div> 
         <div className="col-span-12 flex items-center justify-between">
           <button
-            // onClick={() => {
-            //   const part = formSelections.part
-            //     .replace(/ /g, '_')
-            //     .replace(/-/g, '_')
-            //   const make = formSelections.make
-            //     .replace(/ /g, '_')
-            //     .replace(/-/g, '_')
-            //   const model = formSelections.model
-            //     .replace(/ /g, '_')
-            //     .replace(/-/g, '_')
-            //   const year = formSelections.year
-            //     .replace(/ /g, '_')
-            //     .replace(/-/g, '_')
-            //   const size = formSelections.size
-            //     .replace(/ /g, '_')
-            //     .replace(/-/g, '_')
-            //   router.push(`/search/${part}/${make}/${model}/${year}/${size}`)
-            // }}
-            // href={{ pathname: '/search', query: formSelections }}
-            // disabled={
-            //   formSelections.make === '' ||
-            //   formSelections.model === '' ||
-            //   formSelections.part === '' ||
-            //   formSelections.size === '' ||
-            //   formSelections.year === ''
-            // }
+             onClick={() => {
+              const part = formSelections.part
+               .replace(/ /g, '_')
+                .replace(/-/g, '_')
+               const make = formSelections.make
+                .replace(/ /g, '_')
+              .replace(/-/g, '_')
+             const model = formSelections.model
+                .replace(/ /g, '_')
+                .replace(/-/g, '_')
+              const year = formSelections.year
+                 .replace(/ /g, '_')
+                .replace(/-/g, '_')
+              const size = formSelections.size
+                 .replace(/ /g, '_')
+                 .replace(/-/g, '_')
+              router.push(`/search/${part}/${make}/${model}/${year}/${size}`)
+             }}
+             href={{ pathname: '/search', query: formSelections }}
+             disabled={
+               formSelections.make === '' ||
+               formSelections.model === '' ||
+              formSelections.part === '' ||
+              formSelections.size === '' ||
+              formSelections.year === ''
+            }
 
             className="focus:shadow-outline flex max-h-20 w-full items-center justify-around rounded bg-orange-400 px-4 py-2 text-center font-bold text-white hover:bg-orange-700 focus:outline-none"
             type="submit"
